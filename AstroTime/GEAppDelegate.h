fa7cf78283028a6fb0e67e348bcdf233950a8186
Gary Edgcombe <garyedgcombe@garys-mbp.lan>
2014-04-24 15:19:52 +0100

    Initial Commit

---
+++ AstroTime/GEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GEAppDelegate.h
+//  AstroTime
+//
+//  Created by Gary Edgcombe on 24/04/2014.
+//  Copyright (c) 2014 Gary Edgcombe Code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
